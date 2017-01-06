94ae6ed362846fb7e82dce60ff7b5d1b80b8e0e4
Davor Grubelic <dgrubelic@Davors-MacBook-Pro.local>
2017-01-06 14:48:35 +0100

    added trends

--- src/components/right-sidebar.vue
+++ src/components/right-sidebar.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="right-sidebar">
-    
+    <div class="trends">
+      <div class="sidebar__heading">Philippines trends</div>
+      <a href="#" class="trends__trend">#DONTSTOPLYRICVIDEO</a>
+      <a href="#" class="trends__trend">#MLFTSltsComlicated</a>
+      <a href="#" class="trends__trend">#TangaLangMoments</a>
+      <a href="#" class="trends__trend">#a5WordsForYou</a>
+      <a href="#" class="trends__trend">#5SOSDONTSTOPLYRICVIDEO</a>
+      <a href="#" class="trends__trend">Happy Chicserific Day</a>
+      <a href="#" class="trends__trend">Dr. Fluke</a>
+      <a href="#" class="trends__trend">JaDine For TBYD The Movie</a>
+      <a href="#" class="trends__trend">Galawang JaXel</a>
+      <a href="#" class="trends__trend">JulQuen For OBTCHTheMovie</a>
+    </div><!-- /.trends -->
+
+    <div class="who-to-follow">
+      <div class="sidebar__heading">Who to follow</div><!-- /.sidebar__heading -->
+    </div><!-- /.who-to-follow -->
   </div><!-- /.right-sidebar -->
 </template>
 
@@ -15,6 +31,7 @@
     bottom: 0;
     background-color: #fffbf5;
     right: 0;
+    padding: 40px 30px;
     position: absolute;
     top: 0;
     transition: all .25s;
@@ -22,6 +39,22 @@
 
     @media (max-width: 1200px) {
       transform: translateX(100%);
+    }
+
+    .sidebar__heading {
+      font-weight: 300;
+      margin-bottom: 20px;
+      text-transform: uppercase;
+    }
+
+    .trends {
+      margin-bottom: 40px;
+
+      &__trend {
+        color: #80a8cc;
+        font-weight: 300;
+        text-decoration: none;
+      }
     }
   }
 </style>
